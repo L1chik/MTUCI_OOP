dda520e130cb3f51f8cc8cc465e7dbde97dbf7b7
L1chik <gleb2605@bk.ru>
2020-09-24 00:24:41 +0300

    lab1

--- LAB1/src/Palindrome.java
+++ LAB1/src/Palindrome.java
@@ -1,7 +1,24 @@
 public class Palindrome {
     public static void main(String[] args){
-        for (int i =0; i < args.length; i++){
-            String stroke = args[i];
+        String ll = "java Palindrome madam racecar apple kayak song noon\n";
+        System.out.println(isPalindrome(ll));
+    }
+
+
+//    создание обратной строки
+    public static String reverseString(String stroke){
+        String buff = "";
+        for (int i = stroke.length()-1; i >= 0; i--){
+            char sym = stroke.charAt(i);
+            buff += sym;
         }
+        return buff;
+    }
+
+//    проверка перевернутой с траки с оригинальной
+    public static boolean isPalindrome(String stroke){
+        String reverseStroke = reverseString(stroke);
+        return reverseStroke.equals(stroke);
     }
 }
+
