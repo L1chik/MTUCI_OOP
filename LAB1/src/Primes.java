0a1dd4953df74fc8fd8f6463195ed76502fbc467
L1chik <gleb2605@bk.ru>
2020-09-24 00:23:42 +0300

    lab1

--- LAB1/src/Primes.java
+++ LAB1/src/Primes.java
@@ -1,17 +1,22 @@
 public class Primes {
+
+    //Проход по диапазону, вывод нужных значений
     public static void main(String[] args) {
         int i;
-        for (i = 1; i <= 100; i++) {
+        for (i = 2; i <= 100; i++) {
             if(isPrime(i))
                 System.out.println(i);
         }
-        }
+    }
+
+    //Нахождение наименьшего делителя
     public static boolean isPrime(int n){
+        boolean check = true;
         for (int j=2; j<n; j++){
             if (n % j ==0 ){
-                return false;
+                check = false;
             }
         }
-        return true;
+        return check;
     }
-    }
+}
